481c4a560fb61f36eb49916dd39a7a8141a52f11
Dean Reid <deannreid@hotmail.co.uk>
2023-10-15 23:09:16 +0100

    Add Network

--- src/main.tf
+++ src/main.tf
@@ -30,8 +30,26 @@ provider "azurevm" {
   features {}
 }
 
+### 
+  # XHST Used Locations
+  #  "East US"
+  #  "Central US"
+  #  "North Central US"
+  #  "UK South"
+  #  "UK West"
+  #  "North Europe"
+  #  "West Europe"
+###
 # Resource Group Creation
 resource "azurerm_resource_group" "XHST-Devel-RG" {
   name     = "XHT-Devel-Resources"
   location = "UK South"
+}
+
+# Virtual Network Ceation
+resource "azurerm_virtual_network" "XHST-Devel-VN" {
+  name                = "XHT-Devel-VirtNwk"
+  resource_group_name = azurerm_resource_group.XHST-Dev-RG.name # Create implicit dependency req. 
+  location            = azurerm_resource_group.XHST-Dev-RG.location # Grab Location from Resource so hardcoding is avoided
+  address_space       = ["10.0.15.0/16"] # Dev Space IP Range
 }
